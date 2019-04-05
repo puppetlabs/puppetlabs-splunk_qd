5e0e19685424ac0ea714a88b40219963a9a021fe
Cody Herriges <cody@herriges.org>
2019-04-05 08:16:25 -0700

    Addon install
    	Basic, dumb addon installation

--- site-modules/profiles/plans/splunk/search.pp
+++ site-modules/profiles/plans/splunk/search.pp
@@ -12,4 +12,10 @@ plan profiles::splunk::search() {
     }
     class { 'splunk::enterprise': package_ensure => latest, manage_password => true }
   }
+
+  # Should be moved into Puppet code
+  upload_file('profiles/splunk/addons/puppet-report-viewer_135.tgz', '/tmp/puppet-report-viewer_135.tgz', $searcher, 'Uploading Splunk Addon: Puppet Report Viewer')
+  run_command('/opt/splunk/bin/splunk install app -auth admin:changeme /tmp/puppet-report-viewer_135.tgz', $searcher, '_catch_errors' => true)
+  upload_file('profiles/splunk/addons/puppet-tasks-actionable-alerts-for-splunk_101.tgz', '/tmp/puppet-tasks-actionable-alerts-for-splunk_101.tgz', $searcher, 'Uploading Splunk Addon: Puppet Tasks Actionable Alerts')
+  run_command('/opt/splunk/bin/splunk install app -auth admin:changeme /tmp/puppet-tasks-actionable-alerts-for-splunk_101.tgz', $searcher, '_catch_errors' => true)
 }
