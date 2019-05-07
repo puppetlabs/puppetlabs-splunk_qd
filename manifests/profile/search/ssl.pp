816ebe4da1b0330ae846374ecb69c7c57e9e11f7
Cody Herriges <cody@puppet.com>
2019-05-07 15:03:21 -0700

    Introduce letencrypt for cert provisioning

--- manifests/profile/search/ssl.pp
+++ manifests/profile/search/ssl.pp
@@ -2,9 +2,23 @@ class splunk_qd::profile::search::ssl inherits splunk_qd::profile::search {
 
   splunk_web { 'settings/enableSplunkWebSSL': value => true }
   splunk_web { 'settings/privKeyPath':
-    value => '/opt/splunk/etc/auth/splunkweb/privkey.pem'
+    value => '/etc/letsencrypt/live/splunk-qd-head.gcp.herrig.es/privkey.pem'
   }
   splunk_web { 'settings/serverCert':
-    value => '/opt/splunk/etc/auth/splunkweb/cert.pem'
+    value => '/etc/letsencrypt/live/splunk-qd-head.gcp.herrig.es/cert.pem'
+  }
+
+  class { 'letsencrypt':
+    email             => 'cody@puppet.com',
+    configure_epel    => true,
+    cron_scripts_path => '/opt/splunk/var'
+  }
+
+  letsencrypt::certonly { 'splunk-qd-head.gcp.herrig.es':
+    manage_cron          => true,
+    cron_hour            => [0,12],
+    cron_minute          => '30',
+    cron_success_command => "/bin/systemctl ${splunk::enterprise::service_name} restart",
+    suppress_cron_output => true,
   }
 }
