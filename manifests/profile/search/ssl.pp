c61dda8d43740ac1322d98a346c0b30fc11a1bca
Cody Herriges <193064+ody@users.noreply.github.com>
2019-05-10 15:02:04 -0700

    Reworking SSL settings in plan
    	Allow for inventory vars to be overridden for SSL settings

--- manifests/profile/search/ssl.pp
+++ manifests/profile/search/ssl.pp
@@ -1,24 +1,24 @@
 class splunk_qd::profile::search::ssl(
-  String[1] $external_fqdn                = $fqdn,
-  Enum['internal', 'letsencrypt'] $mode   = 'internal',
-  Optional[String[1]] $registration_email = undef,
+  String[1] $registration_email,
+  String[1] $registration_fqdn          = $fqdn,
+  Enum['internal', 'letsencrypt'] $mode = 'internal',
 ) inherits splunk_qd::profile::search {
 
   class { 'apache': default_vhost => false }
 
-  apache::vhost { "redirect ${external_fqdn} non-ssl":
-    servername      => $external_fqdn,
+  apache::vhost { "redirect ${registration_fqdn} non-ssl":
+    servername      => $registration_fqdn,
     port            => '80',
-    docroot         => "/var/www/${external_fqdn}",
+    docroot         => "/var/www/${registration_fqdn}",
     manage_docroot  => true,
     redirect_status => 'permanent',
     redirectmatch_regexp => '^(/(?!\.well-known/).*)',
-    redirectmatch_dest   => "https://${external_fqdn}/\$1",
+    redirectmatch_dest   => "https://${registration_fqdn}/\$1",
   }
 
   if $mode == 'letsencrypt' {
 
-    $ssl_dir = "/etc/letsencrypt/live/${external_fqdn}"
+    $ssl_dir = "/etc/letsencrypt/live/${registration_fqdn}"
 
     class { 'letsencrypt':
       email             => $registration_email,
@@ -26,15 +26,15 @@ class splunk_qd::profile::search::ssl(
       cron_scripts_path => "${splunk::params::enterprise_homedir}/var",
     }
 
-    letsencrypt::certonly { $external_fqdn:
+    letsencrypt::certonly { $registration_fqdn:
       manage_cron          => true,
       cron_hour            => [0,12],
       cron_minute          => '30',
       cron_success_command => "/bin/systemctl ${splunk::enterprise::service_name} restart",
       suppress_cron_output => true,
-      domains              => [$external_fqdn],
+      domains              => [$registration_fqdn],
       plugin               => 'webroot',
-      webroot_paths        => ["/var/www/${external_fqdn}"],
+      webroot_paths        => ["/var/www/${registration_fqdn}"],
 
     } -> Splunk_web <||>
   } else {
